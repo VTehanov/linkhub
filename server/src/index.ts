045fcea2ef608d4147fa70c9cc0f012af077143f
Vasil Tehanov <vasiltehanov@gmail.com>
2018-12-10 22:51:57 +0200

    Added proper resolver types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,21 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+
 import db from './database';
 import { getTypeDefs, getResolvers } from './createSchema';
+import { Request } from 'express';
 
 
 const server = new GraphQLServer({
   typeDefs: getTypeDefs(),
   resolvers: getResolvers(),
   resolverValidationOptions: {
-    // TODO: fix the need for this
     requireResolversForResolveType: false
   },
-  context: (req: any) => ({
+  context: (req: Request) => ({
     ...req,
     db
   })
 })
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log('* Server is running on http://localhost:4000'))
