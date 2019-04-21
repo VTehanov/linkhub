6075899abe1554f082712e2aa5e04416e1154879
Vasil Tehanov <vasiltehanov@gmail.com>
2019-04-21 16:33:34 +0300

    Added seeding Users and Tags

--- server/src/utils/seedData/index.ts
+++ server/src/utils/seedData/index.ts
@@ -1,22 +1,44 @@
 import * as faker from 'faker'
 import { Project } from '../../entity/Project'
 import { Database } from '../../services/database'
+import { User } from '../../entity/User'
+import { Tag } from '../../entity/Tag'
 
-const seedProjectData = () => {
-  const data = {
-    name: faker.commerce.productName(),
-    description: faker.lorem.paragraph()
-  }
+const seedProjectData = () => ({
+  name: faker.commerce.productName(),
+  description: faker.lorem.paragraph()
+})
 
-  return data
-}
+const tagsData = ['Rest API', 'Statistics', 'Machine Learning']
 
-const seedProjects = async () => {
+const seed = async () => {
   await Database.createConnection()
+
+  const userPromises: Array<Promise<User>> = [
+    User.create({
+      email: 'creator@mail.com',
+      password: '12345678'
+    }).save(),
+    User.create({
+      email: 'test@mail.com',
+      password: '12345678'
+    }).save()
+  ]
+
+  const tagsPromises = tagsData.map(name => Tag.create({ name }).save())
+
+  const [projectCreator] = await Promise.all(userPromises)
+  const tags = await Promise.all(tagsPromises)
 
   const projectPromises: Array<Promise<Project>> = []
   for (let i = 0; i < 10; i++) {
-    projectPromises.push(Project.create(seedProjectData()).save())
+    projectPromises.push(
+      Project.create({
+        ...seedProjectData(),
+        creator: projectCreator,
+        tags
+      }).save()
+    )
   }
 
   Promise.all(projectPromises).then(() =>
@@ -27,4 +49,4 @@ const seedProjects = async () => {
   )
 }
 
-seedProjects()
+seed()
